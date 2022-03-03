78e24a40e831dd7121c4c39ca5edd1b045cf56eb
fyofio <muhammadisrofiordi@gmail.com>
2022-03-03 23:08:32 +0700

    coba nambahin page traversal 1

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -36,7 +36,43 @@ end
 #   end
 # end
 
-#load paginated links
+# load next page
+indeks = page['vars']['i']
+puts indeks
+next_url = page['vars']['root-url']+"?page=#{indeks}" if indeks < 11
+indeks += 1 #page selanjutnya
+
+# input ke pages
+if indeks < 11
+  pages << {
+    page_type: "listings",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: next_url,
+    vars: {
+      category: "Women's clothing",
+      "root-url" => "https://www.aliexpress.com/category/100003109/women-clothing.html",
+      "i" => indeks
+    },
+    fetch_type: "browser",
+    driver: {
+      code: "
+        await page.evaluate('window.scrollBy(0,1200)');
+        await sleep(1000);
+        await page.evaluate('window.scrollBy(0,1200)');
+        await sleep(1000);
+        await page.evaluate('window.scrollBy(0,1200)');
+        await sleep(1000);
+        await page.evaluate('window.scrollBy(0,1200)');
+        await sleep(1000);
+        await page.evaluate('window.scrollBy(0,1200)');
+        await sleep(1000);
+      "
+    }
+  }
+end
+
+
 # pagination_links = nokogiri.css('#pagination-bottom a')
 # pagination_links.each do |link|
 #   l_val = link.text.strip
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,9 @@ pages << {
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
   vars: {
-    category: "Women's clothing"
+    category: "Women's clothing",
+    "root-url"=> "https://www.aliexpress.com/category/100003109/women-clothing.html",
+    "i"=> 2 #indeks buat ke page 2
   },
   fetch_type: "browser",
   driver: {
